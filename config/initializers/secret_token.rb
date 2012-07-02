d152ab64e7c09d5c8a78b9ac9160aab668b71af0
heriberto perez <heriberto.perez@crowdint.com>
2012-07-02 15:53:10 -0500

    commit inicial proyecto virgen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practice::Application.config.secret_token = '92efe721b3a878599e93855deeaaaddb26c00cfb63c4af18d9b52fb0937f90a7988dd8bea78b09c675bd0a5ace313a888e493bc7eb5204881d5a4ba37609b532'
